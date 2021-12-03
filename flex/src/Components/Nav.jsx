96241275edc69bb773be3c5f4e98eff78304c7fb
suraj1270 <surajkumar7050901148@gmail.com>
2021-12-03 18:19:23 +0530

    Layout Created

--- flex/src/Components/Nav.jsx
+++ flex/src/Components/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './nav.css'
+
 
 export const Nav = () => {
     return (
-        <div>
-            
+        <div className="fcontainer">
+           <div className="fitem1" id="">First Box</div>
+           <div className="fitem2" id="">Second Box
+           <h1> hello</h1>
+           <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1>
+        
+           </div>
+           <div className="fitem3" id="">Third Box
+           <h1> hello</h1>
+           <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1> <h1> hello</h1>
+        
+           </div>
+           <div className="fitem4" id="">Fourth Box</div>
         </div>
     )
 }
