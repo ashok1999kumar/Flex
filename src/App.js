98b4aa12d94be975f7932afb3ec71d24326d6416
amarsharma <amarraj364@gmail.com>
2021-12-03 13:16:47 +0530

    update card in center

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Nav } from './Components/Nav'
+import { Card } from './Components/Card'
+export const App = () => {
+    return (
+        <>
+            <Nav></Nav>
+            <Card></Card>
+        </>
+    )
+}
---
+++ src/Components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Card.css'
+
+export const Card = () => {
+    
+    return (
+        <>
+           <div className="maindiv">
+                  <div className="divcards">
+                       <div className="cardpic">
+                            <img src='' alt="" /> 
+                          <p>Amar Sharma</p>
+                       </div>
+                        <div className="card1">
+                             <h5>Title</h5> <br/>
+                              <p>Amar sharma</p><br/>
+                              <h5>Discription</h5>
+                              <p> Abc</p>
+                         </div>
+                    </div>  
+           </div>
+        </>
+    )
+}
