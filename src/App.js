779d2be1e7d0b42bd72808e1e7b4915618b9285b
amarsharma <amarraj364@gmail.com>
2021-12-04 19:04:43 +0530

    update responsive card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Nav } from './Components/Nav'
+
 import { Card } from './Components/Card'
 export const App = () => {
     return (
         <>
-            <Nav></Nav>
             <Card></Card>
         </>
     )
