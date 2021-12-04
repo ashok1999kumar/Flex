2028980be844f4984d0c337f9b6d450c35b3e59c
amarsharma <amarraj364@gmail.com>
2021-12-04 18:39:10 +0530

    update responsive card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,7 +6,8 @@ export const Card = () => {
     return (
         <>
            <div className="maindiv">
-                  <div className="divcards">
+                 
+                 <div className="divcards">
                        <div className="cardpic">
                             <img src='' alt="" /> 
                           <p>Amar Sharma</p>
@@ -16,8 +17,60 @@ export const Card = () => {
                               <p>Amar sharma</p><br/>
                               <h5>Discription</h5>
                               <p> Abc</p>
-                         </div>
+                         </div>                   
                     </div>  
+
+                    <div className="divcards">
+                       <div className="cardpic">
+                            <img src='' alt="" /> 
+                          <p>Amar Sharma</p>
+                       </div>
+                        <div className="card1">
+                             <h5>Title</h5> <br/>
+                              <p>Amar sharma</p><br/>
+                              <h5>Discription</h5>
+                              <p> Abc</p>
+                         </div>                   
+                    </div> 
+
+                    <div className="divcards">
+                       <div className="cardpic">
+                            <img src='' alt="" /> 
+                          <p>Amar Sharma</p>
+                       </div>
+                        <div className="card1">
+                             <h5>Title</h5> <br/>
+                              <p>Amar sharma</p><br/>
+                              <h5>Discription</h5>
+                              <p> bca</p>
+                         </div>                   
+                    </div>                
+                               
+                    <div className="divcards">
+                       <div className="cardpic">
+                            <img src='' alt="" /> 
+                          <p>Amar Sharma</p>
+                       </div>
+                        <div className="card1">
+                             <h5>Title</h5> <br/>
+                              <p>Amar sharma</p><br/>
+                              <h5>Discription</h5>
+                              <p> bca</p>
+                         </div>                   
+                    </div> 
+                    <div className="divcards">
+                       <div className="cardpic">
+                            <img src='' alt="" /> 
+                          <p>Amar Sharma</p>
+                       </div>
+                        <div className="card1">
+                             <h5>Title</h5> <br/>
+                              <p>Amar sharma</p><br/>
+                              <h5>Discription</h5>
+                              <p> bca</p>
+                         </div>                   
+                    </div> 
+
            </div>
         </>
     )
